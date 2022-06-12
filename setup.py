0d6911cdd5ff0434f3774e0cd07da0f6dcab342b
aokuyama <ao980s@gmail.com>
2022-06-12 10:09:20 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="line_notify",
+    version="0.1.0",
+    author="aokuyama",
+    author_email="mail@aokuyama.work",
+    description="line common notify",
+    long_description="line common notify",
+    long_description_content_type="text/markdown",
+    url="https://web.aokuyama.work",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
